35b7a1a8434e67a717e0e279f419ac8cec86c336
Robert Greener <me@r0bert.dev>
2024-02-22 12:57:02 +0000

    Add clipp

--- contagent-bin/src/main.cc
+++ contagent-bin/src/main.cc
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "contagent/contagent.h"
+#include "clipp.h"
 
-int main() {
-  contagent::Named named("hi");
-  std::cout << named.getName() << std::endl;
+using namespace clipp;
+int main(int argc, char* argv[]) {
+  uint_fast32_t start_time;
+  uint_fast32_t end_time;
+
+  auto cli = (
+      option("-s", "--start-time").set(start_time).doc("The start time of the simulation"),
+      option("-e", "--end-time").set(end_time).doc("The end time of the simulation")
+      );
+
+  if (!parse(argc, argv, cli) || true) {
+    std::cout << make_man_page(cli, argv[0]);
+  }
 }
