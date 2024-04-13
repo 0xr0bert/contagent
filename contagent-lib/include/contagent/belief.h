5d6eccdd48c5bff52a7936751b20c0a6790539bf
Robert Greener <dev@greener.sh>
2024-04-13 14:17:48 +0000

    belief.h: Document API
    https://todo.sr.ht/~ragreener1/contagent/8
    Signed-off-by: Robert Greener <dev@greener.sh>
    Approved-by: Robert Greener <dev@greener.sh>

--- contagent-lib/include/contagent/belief.h
+++ contagent-lib/include/contagent/belief.h
@@ -36,36 +36,92 @@
 #include <memory>
 namespace contagent {
 
+/// Belief represents a belief in the simulation that an Agent may hold. It is
+/// both Named and UUIDd.
+/// \author Robert Greener
 class Belief : public UUIDd, public Named {
 public:
+  /// Create a new Belief with a supplied name and a randomly generated UUID.
+  /// \param name The name.
+  /// \author Robert Greener
   explicit Belief(std::string name) noexcept;
 
+  /// Create a new Belief with a supplied name and UUID.
+  /// \param uuid The UUID.
+  /// \param name The name.
+  /// \author Robert Greener
   Belief(boost::uuids::uuid uuid, std::string name) noexcept;
 
+  /// Set the relationship to some new value. This is the value that describes
+  /// how adopting the other belief is affected by holding this belief. 1 is no
+  /// effect. < 1 is a negative influence. > 1 is a positive influence.
+  /// \brief Set the relationship to some new value.
+  /// \param belief The belief related to this.
+  /// \param relationship The new relationship weight.
+  /// \author Robert Greener
   [[maybe_unused]] void setRelationship(std::weak_ptr<Belief> &belief,
                                         double_t relationship) noexcept;
 
+  /// Set all the relationships, overriding the existing ones. This is the value
+  /// that describes how adopting the other belief is affected by holding this
+  /// belief. 1 is no effect. < 1 is a negative influence. > 1 is a positive
+  /// influence.
+  /// \brief Set all the relationships.
+  /// \param relationships The new relationships.
+  /// \author Robert Greener
   [[maybe_unused]] void
   setRelationships(std::map<std::weak_ptr<Belief>, double_t, std::owner_less<>>
                        relationships) noexcept;
 
+  /// Get all the relationships. This is the value
+  /// that describes how adopting the other belief is affected by holding this
+  /// belief. 1 is no effect. < 1 is a negative influence. > 1 is a positive
+  /// influence.
+  /// \brief Get all the relationships.
+  /// \return The relationships.
+  /// \author Robert Greener
   [[nodiscard]] const std::map<std::weak_ptr<Belief>, double_t,
                                std::owner_less<>> &
   getRelationships() const noexcept;
 
+  /// Set the perception. This is the amount that when observing a Behaviour,
+  /// they can be assumes to be driven by this Belief. In a range of [-1,+1].
+  /// \brief Set the perception.
+  /// \param behaviour The Behaviour that could be observed.
+  /// \param perception The new perception. In a range of [-1,+1].
+  /// \author Robert Greener
   [[maybe_unused]] void setPerception(std::weak_ptr<Behaviour> &behaviour,
                                       double_t perception) noexcept;
 
+  /// Sets all the perceptions. This overwrites all the old perceptions. This is
+  /// the amount that when observing a Behaviour, they can be assumes to be
+  /// driven by this Belief. In a range of [-1,+1].
+  /// \brief Set all the perceptions.
+  /// \param perceptions The new perceptions. In a range of [-1,+1].
+  /// \author Robert Greener
   [[maybe_unused]] void
   setPerceptions(std::map<std::weak_ptr<Behaviour>, double_t, std::owner_less<>>
                      perceptions) noexcept;
 
+  /// Gets all the perceptions. This is the amount that when observing a
+  /// Behaviour, they can be assumes to be driven by this Belief. In a range of
+  /// [-1,+1].
+  /// \brief Gets all the perceptions.
+  /// \return The perceptions.
+  /// \author Robert Greener
   [[nodiscard]] const std::map<std::weak_ptr<Behaviour>, double_t,
                                std::owner_less<>> &
   getPerceptions() const noexcept;
 
 private:
+  /// This is the value
+  /// that describes how adopting the other belief is affected by holding this
+  /// belief. 1 is no effect. < 1 is a negative influence. > 1 is a positive
+  /// influence.
   std::map<std::weak_ptr<Belief>, double_t, std::owner_less<>> relationships_;
+
+  /// Perceptions is the amount that when observing a Behaviour,
+  /// they can be assumes to be driven by this Belief. In a range of [-1,+1].
   std::map<std::weak_ptr<Behaviour>, double_t, std::owner_less<>> perceptions_;
 };
 } // namespace contagent
